d491c3b2cda98d57941245cf8a664cd71d765673
fdressler <fdressler@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2006-01-03 15:40:05 +0000

    added lizence headers
    git-svn-id: file:///Users/braun/svn/vermont/trunk/npag@511 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/icmp_module.h
+++ src/icmp_module.h
@@ -1,3 +1,25 @@
+/*
+ * This file is part of
+ * npag - Network Packet Generator
+ * Copyright (C) 2005 Christian Bannes, University of Tübingen,
+ * Germany
+ * 
+ * npag is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+
 void fill_icmphdr		__P((packet_buffer_t*, config_t*));
 void fill_icmp6hdr		__P((packet_buffer_t*, config_t*));
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
+/*
+ * This file is part of
+ * npag - Network Packet Generator
+ * Copyright (C) 2005 Christian Bannes, University of Tübingen,
+ * Germany
+ * 
+ * npag is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
 
 #include <stdio.h>
 #include <pthread.h>
@@ -149,7 +170,7 @@ void wait_for_threads() {
 
 
 static void usage() {
-	printf("Usage: netpag -f [config file] [-options]\n\n" \
+	printf("Usage: npag -f [config file] [-options]\n\n" \
 			"Options: \n"\
 			"\t-v verbose output\n" \
 			"\t-vv even more outout\n"\
